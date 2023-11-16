a0b378001810ca42008be308b8de61bf9960be0b
irfancrudx <apirfan33@gmail.com>
2023-11-16 10:18:07 +0530

    color constant added

--- lib/common/utils/app_color.dart
+++ lib/common/utils/app_color.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
 
-class AppColor{
+class AppColor {
   static const kGreenColor = Color(0xFF11A920);
-  
+
+  // black colores
+  static const kblack = Color(0xFF000000);
+  static const kblack40 = Color(0xFF595959);
+  static const kblack30 = Color(0xFFA5A4AA);
+  static const kblack20 = Color(0xFFECECEE);
+  static const kblack10 = Color(0xFFF4F4F4);
+
+  //
+  static const kLightOrange = Color(0xFFFFEFE1);
+  static const kLightGray = Color(0xFFF6F6F6);
+  static const kGray = Color(0xFFF1F1F1);
+
+  static const ksecondaryGreen = Color(0xFF04B155);
+  static const kSecondaryRed = Color(0xFFFF6565);
 }
